517cbacd7d85d331251a13f6940fca44c089ef2d
Ralf <ralfgvenchbillones@gmail.com>
2021-06-21 05:33:01 +0800

    finished

--- models/models.py
+++ models/models.py
@@ -27,9 +27,7 @@ class ResignationLetter(models.Model):
         )
 
     date = fields.Date(default=fields.Date.today())
-    resignatory_template = fields.Binary(string="Resignatory Template",
-                                        related="employee_id.job_id.signatory_files")
-    resignatory_letter = fields.Binary(string="Upload Resignatory")                                 
+                                   
     
     state = fields.Selection([
         ('draft', 'Draft'), 
@@ -47,11 +45,19 @@ class ResignationLetter(models.Model):
         self.state = 'sent'
 
     def approve(self):
-        # self.state = 'approve'
-        signatory_files = self.env['hr.job'].browse(1).signatory_files
-        if not signatory_files:
-            raise ValidationError('No uploaded template, Please contact your manager')
-        return signatory_files
+        self.state = 'approve'
+        clearance = self.env['clearance.signatory'].create({
+            'employee_id': self.employee_id.id,
+        })
+        action = self.env.ref('employee_exit_management.clearance_signatory_action')
+        return {
+            'res_id': clearance.id,
+            'name': action.name,
+            'type': action.type,
+            'res_model': action.res_model,
+            'view_mode': 'form',
+        }
+
 
     def pending(self):
         self.state = 'pending'
@@ -59,6 +65,7 @@ class ResignationLetter(models.Model):
     def disapprove(self):
         context = {
             'id': self.id,
+            'model': 'resignation.letter'
         }
         return {
             'name': 'Reason why disapproving',
@@ -70,6 +77,9 @@ class ResignationLetter(models.Model):
             'target': 'new',
         }
 
+    
+        
+
 
 class DisapproveModal(models.TransientModel):
     _name = 'modal.reason'
@@ -77,7 +87,8 @@ class DisapproveModal(models.TransientModel):
 
 
     def disapprove(self):
-        resignation_letter = self.env['resignation.letter'].browse(int(self._context['id']))
+        model = self._context['model']
+        resignation_letter = self.env[model].browse(int(self._context['id']))
         resignation_letter.state = 'disapprove'
         resignation_letter.reason_disapprove = self.reason
 
@@ -90,3 +101,54 @@ class HrJob(models.Model):
 
     signatory_files = fields.Binary(string='Signatory Template')
 
+class ClearanceSignatory(models.Model):
+    _name = 'clearance.signatory'
+
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string='Employee',
+        )
+
+    signatory_template = fields.Binary(string="Resignatory Template",
+                                        related="employee_id.job_id.signatory_files")
+    signatory_letter = fields.Binary(string="Upload Resignatory")  
+
+    reason_disapprove = fields.Text(string="Reason/Rejected Message")
+
+
+    state = fields.Selection([
+        ('draft', 'Draft'), 
+        ('sent', 'Sent'), 
+        ('pending', 'Reviewing'),
+        ('approve', 'Approve'),
+        ('disapprove', 'Disapprove')
+        ],
+        default='draft')
+
+
+    def submit(self):
+        if not self.signatory_letter:
+            raise ValidationError('Please upload your signatory letter')
+        self.state = 'sent'
+
+    def approve(self):
+        self.state = 'approve'
+
+
+    def pending(self):
+        self.state = 'pending'
+
+    def disapprove(self):
+        context = {
+            'id': self.id,
+            'model': 'clearance.signatory'
+        }
+        return {
+            'name': 'Reason why disapproving',
+            'id': 'modal_reason_view_form',
+            'res_model': 'modal.reason',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'context': context,
+            'target': 'new',
+        }
